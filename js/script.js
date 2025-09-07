a02aa5bd82508c326f2580a12f668562e4713ff7
st-shourov12 <shourovsc38@gmail.com>
2025-09-07 21:03:00 +0600

    complete display word

--- js/script.js
+++ js/script.js
@@ -4,6 +4,45 @@ const allLevel = () => {
     .then(data => displayLesson(data.data) )
 };
 
+
+
+
+const showLesson = (id) => {
+    fetch(`https://openapi.programming-hero.com/api/level/${id}`)
+    .then(r => r.json())
+    .then(load => displayLevelWord(load.data))
+};
+
+
+const displayLevelWord = (words) =>{
+    const wordContainer = document.getElementById("word-container");
+    wordContainer.innerHTML= "";
+
+    words.forEach(word => {
+        const card = document.createElement("div");
+
+        console.log(word);
+        card.innerHTML = `
+            <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+                <h2 class="font-bold text-2xl">${word.word}
+                </h2>
+                <p class="font-semibold">
+                    Meaning / Pronounciation
+                </p>
+                <h3 class="text-2xl font-medium fb">
+                    ${word.meaning} / ${word.pronounciation}
+                </h3>
+                <div class="flex justify-between items-center">
+                    <button class="btn bg-[#1a91ff10] hover:bg-[#1a91ff80]"><i class="fa-solid fa-circle-info"></i></button>
+                    <button class="btn bg-[#1a91ff10] hover:bg-[#1a91ff80]"><i class="fa-solid fa-volume-high"></i></button>
+                </div>
+            </div>
+        `;
+
+        wordContainer.append(card);
+    });
+}
+
 const displayLesson = (lessons) => {
     const levelContainer = document.getElementById("level-container");
     levelContainer.innerHTML = ``;
@@ -11,7 +50,7 @@ const displayLesson = (lessons) => {
     lessons.forEach(element => {
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-        <button href="" class="btn btn-outline btn-primary">
+        <button onclick="showLesson(${element.level_no})" href="" class="btn btn-outline btn-primary">
             <i class="fa-solid fa-book-open"></i>
             Lesson -${element.level_no}
         </button>
@@ -20,9 +59,6 @@ const displayLesson = (lessons) => {
     });
 
 };
-
-
-
 
 
 
